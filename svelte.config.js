46e1323b87b26a0d5742e5e6e557f0b248d8706f
owen-mitch <78667282+owen-mitch@users.noreply.github.com>
2024-02-13 17:26:34 -0500

    Removed base path

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,7 @@ const config = {
     kit: {
         adapter: adapter({
 			fallback: 'index.html',
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/' : '',
-		}
+		})
     }
 };
 
